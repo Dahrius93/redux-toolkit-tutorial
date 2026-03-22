1828dbdb8121026e2687625f223d402f17b5835c
Dahrius93 <dariolauretta93@gmail.com>
2026-03-22 11:41:48 +0100

    Nav, Cart created

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,46 @@
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+
+const CartContainer = () => {
+  // usiamo useSelector per accedere allo slice cart
+  // slice significa pezzo quindi accediamo a un container che noi abbiamo
+  // chiamato cart dove dentro c'è lo stato del nostro carrello
+  // lo slice cart è definito su cartSlice.js dove viene inizializzato
+  // con la funzione createSlice()
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn">clear cart</button>
+      </footer>
+    </section>
+  );
+};
+
+export default CartContainer;
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartItem = () => {
+  return <div>CartIte</div>;
+};
+
+export default CartItem;
